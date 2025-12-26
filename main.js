6916cf1393c0fa4575f9b59646034f02b6f819ae
Tejas <kesariatejas@gmail.com>
2025-12-26 20:08:08 +0530

    updated animations for mobile

--- main.js
+++ main.js
@@ -10,8 +10,80 @@ ScrollSmoother.create({
 
 let isShortHeight = window.screen.height < 1050;  
 
+//Content animations
+gsap.from('#section2 .content-wrapper', {
+      scrollTrigger: {
+        trigger: '#section2',
+        start: '-50% bottom',
+        end: 'center center',
+        scrub: true,
+      },
+      y: '140%',
+      ease: 'power1.inOut',
+    });
+
+    gsap.from('#section3 .heading', {
+      scrollTrigger: {
+        trigger: '#section3',
+        start: 'top bottom',
+        end: 'center bottom',
+        scrub: true,
+      },
+      y: '140%',
+      ease: 'power1.inOut',
+    });
+
+    gsap.from('#section4 img', {
+      scrollTrigger: {
+        trigger: '#section4',
+        start: 'top bottom',
+        end: 'center center',
+        scrub: true,
+      },
+      width: 0,
+      opacity: 0,
+      ease: 'power1.inOut',
+    });
+
+    gsap.from('#section6 .content-wrapper', {
+      scrollTrigger: {
+        trigger: '#section6',
+        start: 'top bottom',
+        end: 'center center',
+        scrub: true,
+      },
+      y: '40%',
+      duration: 2,
+      ease: 'power1.inOut',
+    });
+
+    // hero section text animation
+    let split = SplitText.create('#section1 .heading', {
+      type: 'chars, words, lines',
+      mask: 'lines'
+    });
+
+    gsap.from(split.chars, {
+      yPercent: ()=> gsap.utils.random(-100, 100),
+      rotation: ()=> gsap.utils.random(-30, 30),
+      autoAlpha: 0,
+      ease: 'back.out(1.5)',
+      stagger: {
+        amount: 0.5,
+        from: 'random'
+      },
+      duration: 1.5
+    });
+ gsap.from('#headphone', {
+      opacity: 0,
+      scale: 0,
+      duration: 1,
+      delay: 1,
+      ease: 'power1.inOut'
+})
+
 ScrollTrigger.matchMedia({
-  "(min-width: 991px)": function () {
+  "(min-width: 1366px)": function () {
 
     gsap.to('#headphone', {
       scrollTrigger: {
@@ -84,74 +156,62 @@ ScrollTrigger.matchMedia({
     });
   }
 })
-//Content animations
-gsap.from('#section2 .content-wrapper', {
+//ScrollTrigger for mobile devices
+ScrollTrigger.matchMedia({
+  "(max-width: 768px)": function () {
+    gsap.to('#headphone', {
       scrollTrigger: {
         trigger: '#section2',
-        start: '-50% bottom',
+        start: 'top bottom',
         end: 'center center',
         scrub: true,
       },
-      y: '140%',
+      y: '85vh',
+      x: '18vw',
+      width: '32vw',
+      rotate: 90,
       ease: 'power1.inOut',
+      immediateRender: false
     });
-
-    gsap.from('#section3 .heading', {
+     gsap.to('#headphone', {
       scrollTrigger: {
         trigger: '#section3',
         start: 'top bottom',
-        end: 'center bottom',
+        end: 'bottom bottom',
         scrub: true,
       },
-      y: '140%',
+      y: '218vh',
+      x: '0',
+      width: '35vw',
+      rotate: 35,
       ease: 'power1.inOut',
+      immediateRender: false
     });
-
-    gsap.from('#section4 img', {
+      gsap.to('#headphone', {
       scrollTrigger: {
         trigger: '#section4',
         start: 'top bottom',
         end: 'center center',
         scrub: true,
       },
-      width: 0,
-      opacity: 0,
+      y: '308vh',
+      width: '42vw',
+      rotate: 0,
       ease: 'power1.inOut',
+      immediateRender: false
     });
-
-    gsap.from('#section6 .content-wrapper', {
+     gsap.to('#headphone', {
       scrollTrigger: {
-        trigger: '#section6',
+        trigger: '#section5',
         start: 'top bottom',
-        end: 'center center',
+        end: 'center bottom',
         scrub: true,
       },
-      y: '40%',
-      duration: 2,
+      y: '400vh',
+      width: '28vw',
       ease: 'power1.inOut',
+      immediateRender: false
     });
 
-    // hero section text animation
-    let split = SplitText.create('#section1 .heading', {
-      type: 'chars, words, lines',
-      mask: 'lines'
-    });
-
-    gsap.from(split.chars, {
-      yPercent: ()=> gsap.utils.random(-100, 100),
-      rotation: ()=> gsap.utils.random(-30, 30),
-      autoAlpha: 0,
-      ease: 'back.out(1.5)',
-      stagger: {
-        amount: 0.5,
-        from: 'random'
-      },
-      duration: 1.5
-    });
- gsap.from('#headphone', {
-      opacity: 0,
-      scale: 0,
-      duration: 1,
-      delay: 1,
-      ease: 'power1.inOut'
-    })
+  }
+});
